ace3fa4c8768fa2de2804f64542ffe9d26340024
shubhams3423 <shubhams1671h@gmail.com>
2022-11-30 22:48:53 +0530

    added new javascript

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 
-let text=document.querySelector('#text')//we are creating selector
-let todo= document.querySelector('.todo')//we are creating selector
+// let text=document.querySelector('#text')//we are creating selector
+// let todo= document.querySelector('.todo')//we are creating selector
+
+ 
+// let text1 = document.querySelector("#text")
+// let todoapp = document.querySelector(".todoapp")
+
+// text.addEventListener(
+//     "keyup",
+//     function(event){
+//       if(event.key=="Enter")
+//       {
+//         console.log(this.value)//this refer to text
+//         this.value=('')
+//       }
+//     }
+// )
+
+let text=document.querySelector('#text')
+let todoapp=document.querySelector('.todo')
 
 text.addEventListener(
-    "keyup",
-    function(event){
-        //geting data on element that 
-// console.log(event.key)
-if(event.key=="Enter")
-{
-    addTodo(this.value)//"this" represent(refer) the text
-    this.value =''
-}
-}
+  "keyup",
+  function (event){
+    if(event.key=="Enter" ){
+    addtodo(this.value)
+      this.value=('')
+    }
+  }
 )
+  const addtodo =(text) =>{
+    const listitem =document.createElement("li");
+    listitem.innerHTML=`
+    ${text}
+    <i class="fa-regular fa-circle-xmark"></i>`;
+    listitem.querySelector("i").addEventListener(
+      "click",
+      function()
+      {
+        listitem.remove()
+      }
+    )
+    todoapp.appendChild(listitem)
+  }
+
  
-const addTodo = (text) =>{
-    const listitem = 
-     document.createElement('li')
-    listitem.innerHTML = 
-      ${text}  
-      <i class="fa-regular fa-circle-xmark"></i>
-    ;
-    todo.appendChild(listitem)
-}
